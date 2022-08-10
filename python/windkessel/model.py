f20f8547e16c10f2e0d9bdf42f9ee0b9bb2962f2
VanessB <vanessbut@yandex.ru>
2022-08-10 13:29:49 +0300

    Добавлено больше примеров.

--- python/windkessel/model.py
+++ python/windkessel/model.py
@@ -4,6 +4,10 @@ from scipy.interpolate import interp1d
 
 
 def calc_der(T, X):
+    """
+    Вычисление первой производной по времени.
+    """
+
     der_X = numpy.zeros_like(X)
 
     # Производные на границах.
@@ -15,6 +19,15 @@ def calc_der(T, X):
         der_X[index] = (X[index + 1] - X[index - 1]) / (T[index + 1] - T[index - 1])
 
     return der_X
+
+
+
+def loop_function(func, x_min, x_max):
+    """
+    Зацикливание одномерной функции.
+    """
+
+    return lambda x : func((x - x_min) % (x_max - x_min) + x_min)
 
 
 
@@ -52,8 +65,8 @@ class WindkesselModel(torch.nn.Module):
         self.P = new_P
         self.der_P = calc_der(self.T, self.P)
 
-        self.get_P = interp1d(self.T, self.P)
-        self.get_der_P = interp1d(self.T, self.der_P)
+        self.get_P = loop_function(interp1d(self.T, self.P), numpy.min(self.T), numpy.max(self.T))
+        self.get_der_P = loop_function(interp1d(self.T, self.der_P), numpy.min(self.T), numpy.max(self.T))
 
 
     def set_Q_in(self, new_T, new_Q_in):
@@ -65,8 +78,8 @@ class WindkesselModel(torch.nn.Module):
         self.Q_in = new_Q_in
         self.der_Q_in = calc_der(self.T, self.Q_in)
 
-        self.get_Q_in = interp1d(self.T, self.Q_in)
-        self.get_der_Q_in = interp1d(self.T, self.der_Q_in)
+        self.get_Q_in = loop_function(interp1d(self.T, self.Q_in), numpy.min(self.T), numpy.max(self.T))
+        self.get_der_Q_in = loop_function(interp1d(self.T, self.der_Q_in), numpy.min(self.T), numpy.max(self.T))
 
 
     def P_rhs(self, t, P):
